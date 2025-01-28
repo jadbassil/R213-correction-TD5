774d2ca5576652ea1b7727ea9ec491a0deb616d5
jadbassil <31135113+jadbassil@users.noreply.github.com>
2025-01-28 12:34:48 +0100

    Comment code

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -3,22 +3,25 @@ import { formatDate } from "./utils.mjs";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 
+
+
 export async function getEvents() {
-    const today = new Date().toISOString();
+    const today = new Date().toISOString(); // Récupérer la date du jour. Regardez la doc de la méthode toISOString pour plus d'informations: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
     let events = await pb.collection("events").getFullList(
         {
             sort: "-date",
             order: "desc",
             filter: `date >= "${today}"`,
         }
-    );
-    events.forEach((event) => {
+    ); // Filtrer les événements passés en fonction de la date. Si la colonne date n'est pas dans votre collection, vous pouvez la remplacer par une autre colonne de type date.
+    events = events.map((event) => {
         event.img = pb.files.getURL(event, event.imgUrl);
-    });
-    events.forEach((event) => {
+        return event;
+    }); // Récupérer l'URL de l'image de chaque événement. Vous pouvez remplacer imgUrl par le nom de la colonne qui contient le nom de l'image.
+    events = events.map((event) => {
         event.date = formatDate(event.date);
-    });
-    console.log(events);
+        return event;
+    }); // Formater la date de chaque événement. Vous pouvez remplacer date par le nom de la colonne qui contient la date de l'événement. La fonction formatDate est importée du fichier utils.mjs.
     return events;
 }
 
